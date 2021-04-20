392abb789edcb72b58eeea9fb647a0f6500546b7
artofmayhem <chef_tony_miller@yahoo.com>
2021-04-20 13:09:06 -1000

    ANIMATION COMPLETE FTW!

--- src/App.js
+++ src/App.js
@@ -1,31 +1,63 @@
 import './App.scss';
 import "bootstrap/dist/css/bootstrap.css";
-import AppBar from '@material-ui/core/AppBar'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import AnimationCanvas from './components/animationcanvas'
-import { Suspense} from "react";
+import {Suspense} from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
 
 function App() {
+    const classes = useStyles();
     return (
         <div className="animation-wrapper">
             <AppBar className='d-flex justify-content-center'
-                    style={{backgroundColor: 'black', padding: '3vh 5vw'}}
+                    style={{backgroundColor: 'white'}}
             >
+                <Toolbar className='d-flex justify-content-between flex-row flex-wrap'>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <h2 style={{alignSelf: 'center'}}>Syntech Industries</h2>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
 
-                <h1 style={{alignSelf: 'center'}}>Syntech Industries</h1>
             </AppBar>
+
             <Suspense fallback={<CircularProgress/>}>
                 <AnimationCanvas/>
             </Suspense>
             <div className='d-flex justify-content-center flex-column align-content-center'
-                 style={{backgroundColor: 'black', padding: '20vh 0', textAlign: 'center'}}>
-                <h2 style={{color: '#AAA', alignSelf: 'center', margin: '0 auto'}}>Experience the Future of 3D...</h2>
-                <h3 style={{color: '#AAA', alignSelf: 'center', margin: '5vh auto'}}>Today</h3>
+                 style={{backgroundColor: 'white', padding: '20vh 0', textAlign: 'center'}}>
+                <h5 style={{color: '#AAA', alignSelf: 'center', margin: '0 auto'}}>Experience the Future of 3D...</h5>
+                <h3 style={{
+                    color: '#AAA',
+                    alignSelf: 'center',
+                    margin: '5vh auto',
+                    padding: '2vh 3vw',
+                    backgroundColor: '#444',
+                    opacity: '0.6',
+                    border: '1px solid #222',
+                    boxShadow: '0 0 1.5vh #AAA'
+                }}>Today</h3>
             </div>
-            <div className='jumbotron-fluid content-box'>
-                <button>Home</button>
-            </div>
-
         </div>
     );
 }
--- src/components/animationcanvas.js
+++ src/components/animationcanvas.js
@@ -6,14 +6,31 @@ import {OrbitControls} from "@react-three/drei";
 function AnimationCanvas() {
     return (
         <Canvas
-            colorManagement={false}
+            colorManagement
             camera={{position: [100, 10, 0], fov: 40}}
         >
+            <ambientLight intensity={0.3} />
+            <directionalLight
+                position={[0, 10, 0]}
+                intensity={1}
+                shadow-mapSize-width={1024}
+                shadow-mapSize-height={1024}
+                shadow-camera-far={50}
+                shadow-camera-left={-10}
+                shadow-camera-right={10}
+                shadow-camera-top={10}
+                shadow-camera-bottom={-10}
+            />
+            <pointLight position={[-10, 0, -20]} intensity={0.5}/>
+            <pointLight position={[0, -10, 0]} intensity={1.5}/>
             <Suspense fallback={null}>
                 <Points/>
             </Suspense>
             {/*<CameraControls />*/}
-            <OrbitControls />
+            <OrbitControls
+            autoRotate={true}
+            autoRotateSpeed={0.3}
+            />
         </Canvas>
     )
 }
